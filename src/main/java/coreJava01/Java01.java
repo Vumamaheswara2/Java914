56235bfe1b45d3525b41cbe58a119a6edd6e7337
Vumamaheswara <omkarreddy2@gmail.com>
2024-09-17 00:35:31 +0530

    java916

---
+++ src/main/java/coreJava01/Java01.java
@@ -0,0 +1,26 @@
+package coreJava01;
+
+public class Java01 {
+    String test;
+
+
+    /*
+     * What is the class: Class is a blueprint of the template, 
+     * class contains, methods,  blocks cons
+     * What is object: all physical enties and instance of a class
+     * Data types: 8 primitive data types
+     * byte,short,int,long
+     * Floating points numbers: float, double
+     * characters: char
+     * Boolean: boolean
+     * 
+     * Wrapper Types
+     * Number: Byte, Short, Integer, Long
+     * Floating: Float, Double
+     * Characters: Charater
+     * Boolean: Boolean
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
