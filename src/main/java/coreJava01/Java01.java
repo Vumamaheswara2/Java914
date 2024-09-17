b6e76c60cbf9a4e567105b663e5fc4c8b41d041b
Vumamaheswara <omkarreddy2@gmail.com>
2024-09-18 00:11:35 +0530

    java18

--- src/main/java/coreJava01/Java01.java
+++ src/main/java/coreJava01/Java01.java
@@ -1,13 +1,11 @@
 package coreJava01;
 
 public class Java01 {
-    String test;
-
 
     /*
      * What is the class: Class is a blueprint of the template, 
      * class contains, methods,  blocks cons
-     * What is object: all physical enties and instance of a class
+     * What is object: all physical entries and instance of a class
      * Data types: 8 primitive data types
      * byte,short,int,long
      * Floating points numbers: float, double
@@ -17,10 +15,88 @@ public class Java01 {
      * Wrapper Types
      * Number: Byte, Short, Integer, Long
      * Floating: Float, Double
-     * Characters: Charater
+     * Characters: Charter
      * Boolean: Boolean
+     *
+     * Assignment Operators
+     *
      */
     public static void main(String[] args) {
         System.out.println("Test");
+        Java01 java = new Java01();
+        //java.assignmentOperators();
+        //java.relationalOperators();
+        java.unaryOperators();
+
+
+    }
+
+    public  void unaryOperators(){
+
+        int a = 10;
+        int b = 20;
+
+        a++;
+        System.out.println(a);
+        b--;
+        System.out.println(b);
+        ++b;
+        System.out.println(b);
+        --a;
+        System.out.println(a);
+    }
+
+    public void relationalOperators(){
+        /*
+        < > <= >= == !=
+         */
+        int a = 10;
+        int b = 5;
+        int c = 10;
+        System.out.println("<");
+        System.out.println(a<b);
+        System.out.println(a<c);
+        System.out.println(b<c);
+
+        System.out.println();
+        System.out.println(">");
+        System.out.println(a>b);
+        System.out.println(a>c);
+        System.out.println(b>c);
+
+        System.out.println();
+        System.out.println(">=");
+        System.out.println(a>=b);
+        System.out.println(a>=c);
+        System.out.println(b>=c);
+
+        System.out.println();
+        System.out.println("<=");
+        System.out.println(a<=b);
+        System.out.println(a<=c);
+        System.out.println(b<=c);
+
+        System.out.println();
+        System.out.println("==");
+        System.out.println(a==b);
+        System.out.println(a==c);
+        System.out.println(b==c);
+
+        System.out.println();
+        System.out.println("!=");
+        System.out.println(a!=b);
+        System.out.println(a!=c);
+        System.out.println(b!=c);
+    }
+
+    public void assignmentOperators(){
+        int a =11;
+        int b =12;
+
+        System.out.println(a+=1);
+        a+=1;
+        System.out.println(a);
+        System.out.println(a+=b);
+        System.out.println(a);
     }
 }
